4d74e0e4de651e2b2619fde7cb03d78626c53f04
Po-Hsun <pohsun@infinitiesoft.com>
2016-10-01 15:13:59 +0800

    update CORSResponseFilter

--- keystone4j-core/src/main/java/com/infinities/keystone4j/CORSResponseFilter.java
+++ keystone4j-core/src/main/java/com/infinities/keystone4j/CORSResponseFilter.java
@@ -18,25 +18,32 @@ package com.infinities.keystone4j;
 
 import java.io.IOException;
 
+import javax.annotation.Priority;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Priority(1000)
 public class CORSResponseFilter implements ContainerResponseFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(CORSResponseFilter.class);
+
 
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-
+		logger.debug("filter cors");
 		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
 
 		headers.add("Access-Control-Allow-Origin", "*");
 		// headers.add("Access-Control-Allow-Origin",
 		// "http://podcastpedia.org"); //allows CORS requests only coming from
 		// podcastpedia.org
-		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, PATCH");
 		headers.add("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, ,X-Subject-Token, X-Auth-Token");
 		headers.add("Access-Control-Expose-Headers", "X-Requested-With, Content-Type, ,X-Subject-Token, X-Auth-Token");
 	}
-
 }
